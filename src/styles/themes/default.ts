0db86d122188cad57383aaf05393df2420cb4d75
Natally <natallyriqueto@hotmail.com>
2023-06-08 16:47:08 -0400

    initializing project netflix

---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = {
+	white: 'rgb(255,255,255)',
+
+	'red': 'rgb(229, 9, 20)',
+
+	'gray-200': 'rgba(255, 255, 255, 0.7)',
+	'gray-300': '#737373',
+	'gray-400': 'rgb(35,35,35)',
+	'gray-600': 'rgb(45,45,45)',
+
+};
