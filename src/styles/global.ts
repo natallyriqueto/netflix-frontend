eb2ca9ebe013515da3e8349320657a8a596447cd
Natally <natallyriqueto@hotmail.com>
2024-01-29 18:42:28 -0500

    fix type error

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -9,7 +9,7 @@ export const GlobalStyle = createGlobalStyle`
 
     :focus {
         outline: 0;
-        box-shadow: 0 0 0 2px ${props => props.theme.white};
+        box-shadow: 0 0 0 2px ${(props) => props.theme.white};
     }
 
     body {
